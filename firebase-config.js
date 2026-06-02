3ae613b9e0b4081b3d4dc5209a2c5a2ee3b892de
Phạm Hồng Sơn <phamhongson063@gmail.com>
2026-06-02 21:37:49 +0900

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAS8YdZW39k0jD0ZFPexeWgzPJ199IFlNI",
+  authDomain: "my-bjt.firebaseapp.com",
+  projectId: "my-bjt",
+  storageBucket: "my-bjt.firebasestorage.app",
+  messagingSenderId: "300416050800",
+  appId: "1:300416050800:web:fe4230a1d209a5582576b5",
+};
